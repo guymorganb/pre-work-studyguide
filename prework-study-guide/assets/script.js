37e20f90bcb99a87bc548a20e5fbfad3e39062c9
Guy Morganb <guymorganb@gmail.com>
2023-03-03 16:24:53 -0600

    Re-merge main

--- prework-study-guide/assets/script.js
+++ prework-study-guide/assets/script.js
@@ -1,19 +1,26 @@
 var topics = ["HTML", "CSS", "Git", "JavaScript"];
+var randomTopic = topics[Math.floor(Math.random() * topics.length)];
 
-for (let x = 0; x < topics.length; x++){
-    console.log(topics[x])
+function listTopics(){
+    for (let x = 0; x < topics.length; x++){
+        console.log(topics[x])
+    }
 }
 
-/*
-if (topic === "HTML"){
-    console.log("Lets study HTML!");
-}else if (topic === "CSS"){
-    console.log("Lets study CSS!");
-}else if (topic === "Git"){
-    console.log("Lets study Git!");
-}else if (topic === "JavaScript"){
-    console.log("Lets study JavaScript!");
-}else {
-    console.log("Please try again!");
+function selectTopic(){
+    if (randomTopic === "HTML"){
+        console.log("Lets study HTML!");
+    }else if (randomTopic === "CSS"){
+        console.log("Lets study CSS!");
+    }else if (randomTopic === "Git"){
+        console.log("Lets study Git!");
+    }else if (randomTopic === "JavaScript"){
+        console.log("Lets study JavaScript!");
+    }else {
+        console.log("Please try again!");
+    }
 }
-*/
+console.log('Here are the topics we learned through Prework:');
+listTopics();
+console.log('Which topic should we study first?');
+selectTopic();
